fa606841611a107c30ba2bee9760fef336409a4e
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2013-10-22 15:38:00 +1300

    Silence Notices if string offset beyond end of string
    When building the fragment to switch on, silence Notices arising from the
    offset going beyond the end of the string.

--- tcpdi_parser.php
+++ tcpdi_parser.php
@@ -785,7 +785,7 @@ class tcpdi_parser {
 				break;
 			}
 			default: {
-				$frag = $data{$offset} . $data{$offset+1} . $data{$offset+2} . $data{$offset+3};
+				$frag = $data{$offset} . @$data{$offset+1} . @$data{$offset+2} . @$data{$offset+3};
 				switch ($frag) {
 					case 'endo':
 						// indirect object
